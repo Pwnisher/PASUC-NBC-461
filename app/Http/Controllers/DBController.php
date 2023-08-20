0c53f8e11365bb54d28dd031e31ee33d646d4b96
John Reign Encina <copythatencina24@gmail.com>
2023-08-20 15:59:20 +0800

    Update DBController.php

--- app/Http/Controllers/DBController.php
+++ app/Http/Controllers/DBController.php
@@ -9,9 +9,25 @@ use App\Models\User;
 
 class DBController extends Controller
 {
-    public function index()
+    public function getEqar()
     {
-        return DB::select("select * from user");
+        $eqarFiles = DB::select("SELECT eqar_id, title, inclusive_date, accomplishment_name, department_section, qar_type, date_submitted, status, is_applied FROM eqars");
+        
+        return view('eqar', ['eqarFiles' => $eqarFiles]);
+    }
+
+    public function updateApplied($eqarId)
+    {
+        try {
+            // Update the is_applied field in the database for the given eqar_id
+            DB::table('eqar_files')
+                ->where('eqar_id', $eqarId)
+                ->update(['is_applied' => true]);
+
+            return response()->json(['success' => true]);
+        } catch (\Exception $e) {
+            return response()->json(['success' => false, 'error' => $e->getMessage()]);
+        }
     }
 
 }
