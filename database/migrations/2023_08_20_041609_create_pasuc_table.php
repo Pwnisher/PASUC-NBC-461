36f78474cf73eec63dd3e2b74c75ae55e096aec8
SpaghettiWolf21 <142078415+SpaghettiWolf21@users.noreply.github.com>
2023-08-20 12:49:29 +0800

    Updated Pasuc Table

--- database/migrations/2023_08_20_041609_create_pasuc_table.php
+++ database/migrations/2023_08_20_041609_create_pasuc_table.php
@@ -20,9 +20,10 @@ return new class extends Migration
             $table->string('eval_status', 45);
             $table->tinyInteger('is_submitted');
 
-            $table->foreign(['eqar_files_eqar_id', 'eqar_files_user_user_id'])
-                  ->references(['eqar_id', 'user_user_id'])->on('eqar_files')
-                  ->onDelete('NO ACTION')->onUpdate('NO ACTION');
+            $table->foreign('eqar_files_eqar_id')->references('eqar_id')->on('eqar_files')
+            ->onDelete('NO ACTION')->onUpdate('NO ACTION');
+             $table->foreign('eqar_files_user_user_id')->references('user_user_id')->on('eqar_files')
+            ->onDelete('NO ACTION')->onUpdate('NO ACTION');
         });
     }
 
