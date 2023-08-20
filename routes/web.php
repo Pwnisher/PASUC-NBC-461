dc15a577c6eb4d2964a268e01ffa9c8c8227ca66
John Reign Encina <copythatencina24@gmail.com>
2023-08-20 10:18:23 +0800

    Added routing ajax

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AddController;
 use App\Http\Controllers\TabController;
-
+use App\Http\Controllers\TableController;
+use Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,9 +20,20 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/home', [TabController::class, 'home']);
-Route::get('/eqar', [TabController::class, 'eqar']);
-Route::get('/eqar', 'TableController@fetchData');
+Route::get('/home', [TabController::class, 'home']);// Use the TabController's eqar method for GET requests
+// Use the TableController's fetchData method for POST requests
+Route::post('/eqar', [TableController::class, 'fetchData']);
+
+// To handle both GET and POST requests on the same route
+Route::match(['get', 'post'], '/eqar', function (Request $request) {
+    if ($request->isMethod('get')) {
+        // Handle GET request using TabController's eqar method
+        return app(TabController::class)->eqar($request);
+    } else {
+        // Handle POST request using TableController's fetchData method
+        return app(TableController::class)->fetchData($request);
+    }
+});
 
 Route::get('/application', [TabController::class, 'application'])->name('application');
 Route::get('/application/{any}', [TabController::class, 'show'])->where('any', '.*');
