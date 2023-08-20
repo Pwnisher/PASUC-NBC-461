a3d409a287cbe3d4c4df3014c50ae7c2f338d9d1
John Reign Encina <copythatencina24@gmail.com>
2023-08-20 10:25:03 +0800

    Update TableController.php

--- app/Http/Controllers/TableController.php
+++ app/Http/Controllers/TableController.php
@@ -6,7 +6,23 @@ use Illuminate\Http\Request;
 
 class TableController extends Controller
 {
-    function fetchData() {
+    function fetchData(Request $request) {
+        //tentative contents
+        
+        $page = $request->input('page', 1);
+        $perPage = 10; // Number of records per page
+        $model = "Eqar";
 
+        $query = $model::query();
+
+        // Apply search filters if applicable
+        if ($request->has('search')) {
+            $search = $request->input('search');
+            $query->where('column_name', 'LIKE', "%$search%");
+        }
+
+        $data = $query->paginate($perPage, ['*'], 'page', $page);
+
+        return response()->json($data);
     }
 }
