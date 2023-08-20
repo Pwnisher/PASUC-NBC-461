35f1c04236969880ef0ea47b914f7ada143f8cbe
SpaghettiWolf21 <142078415+SpaghettiWolf21@users.noreply.github.com>
2023-08-20 10:52:53 +0800

    Add the Controller and updated the Seeders

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\AddController;
 use App\Http\Controllers\TabController;
 use App\Http\Controllers\TableController;
 use Illuminate\Http\Request;
+use App\Http\Controllers\DBController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,21 +22,10 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/home', [TabController::class, 'home']);// Use the TabController's eqar method for GET requests
-// Use the TableController's fetchData method for POST requests
-Route::post('/eqar', [TableController::class, 'fetchData']);
+Route::get('DBController', [DBController::class, 'index']);
 
-// To handle both GET and POST requests on the same route
-Route::match(['get', 'post'], '/eqar', function (Request $request) {
-    if ($request->isMethod('get')) {
-        // Handle GET request using TabController's eqar method
-        return app(TabController::class)->eqar($request);
-    } else {
-        // Handle POST request using TableController's fetchData method
-        return app(TableController::class)->fetchData($request);
-    }
-});
-
+Route::get('/home', [TabController::class, 'home']);
+Route::get('/eqar', [TabController::class, 'eqar']);
 Route::get('/application', [TabController::class, 'application'])->name('application');
 Route::get('/application/{any}', [TabController::class, 'show'])->where('any', '.*');
 
