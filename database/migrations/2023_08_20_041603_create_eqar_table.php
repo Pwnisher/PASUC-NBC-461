2b69087a674d252c5c7257ac72055d9b68dc3633
princessvillavilla <123225085+princessvillavilla@users.noreply.github.com>
2023-08-21 20:32:19 +0800

    Create 2023_08_20_041603_create_eqar_table.php

---
+++ database/migrations/2023_08_20_041603_create_eqar_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('eqars', function (Blueprint $table) {
+            $table->id('eqar_id');
+            $table->string('user_user_id');
+            $table->tinyInteger('is_applied');
+            $table->string('file_path')->nullable();
+            $table->string('title');
+            $table->date('inclusive_date');
+            $table->string('accomplishment_name');
+            $table->string('department_section');
+            $table->string('qar_type');
+            $table->date('date_submitted');
+            $table->string('status')->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_user_id')->references('user_id')->on('users')
+                  ->onDelete('NO ACTION')->onUpdate('NO ACTION');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('eqars');
+    }
+};
