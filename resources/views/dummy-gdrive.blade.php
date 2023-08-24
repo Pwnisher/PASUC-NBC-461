d3294d2c7580baef21f7a6a789aac6a3cacea8e1
John Reign Encina <copythatencina24@gmail.com>
2023-08-24 18:57:03 +0800

    test

---
+++ resources/views/dummy-gdrive.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--For Vite Dev-->
+    @vite('resources/css/app.css')
+    <!--Font Awesome Library (For icons)-->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
+    <!--Favicon-->
+    <link rel="icon" type="image/png" href="{{ asset('favicon.ico')}}">
+    
+    <title>GDrive Dummy Experiment</title>
+</head>
+<body class="flex items-center justify-center h-screen">
+    <!-- Upload Documents Form -->
+    <div class="bg-white p-8 rounded shadow-md w-80">
+        <h1 class="text-2xl font-semibold mb-4">Upload Documents</h1>
+        <form id="uploadForm" class="space-y-4" action="{{ route('uploadToDrive') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="flex flex-col">
+                <label for="fileInput" class="text-sm font-medium mb-1">Choose a file</label>
+                <input type="file" id="fileInput" name="file" class="py-2 px-3 border rounded">
+            </div>
+            <button type="submit" class="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600 transition duration-300">Upload</button>
+        </form>
+    </div>
+
+    <script>
+        const form = document.getElementById('uploadForm');
+        form.addEventListener('submit', async (event) => {
+            event.preventDefault();
+            
+            const formData = new FormData(form);
+            try {
+                const response = await fetch(form.action, {
+                    method: 'POST',
+                    body: formData,
+                });
+                
+                // Handle response from server if needed
+                const data = await response.json();
+                console.log(data);
+            } catch (error) {
+                console.error('Error uploading file:', error);
+            }
+        });
+    </script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\DBController;
 use App\Http\Controllers\SearchController;
 use App\Http\Controllers\SortController;
 use App\Http\Controllers\CycleController;
-
+use App\Http\Controllers\GoogleDriveController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,32 +22,38 @@ use App\Http\Controllers\CycleController;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('dummy_gdrive');
 });
 
-Route::get('DBController', [DBController::class, 'store']);
-
-Route::get('/home', [TabController::class, 'home']);
-
-Route::get('/home', [DBController::class, 'home']);
-Route::get('/eqar', [DBController::class, 'eqar']);
-Route::get('/eqar', [DBController::class, 'getEqar'])->name('eqar');
-Route::put('/eqar-update-applied/{eqarId}', [DBController::class, 'eqarUpdateApplied']);
-Route::get('/eqarSearch', [SearchController::class, 'eqarSearch']);
-Route::get('/eqarSort', [SortController::class, 'eqarSort']);
-Route::get('/pasucSearch', [SearchController::class, 'pasucSearch']);
-Route::get('/pasucSort', [SortController::class, 'pasucSort']);
-Route::get('/cycle', [CycleController::class, 'cycle']);
+Route::get('/dummy_gdrive', [GoogleDriveController::class, 'dummy_gdrive']);
+Route::get('/oauth2callback', [GoogleDriveController::class, 'oauth2callback']);
+Route::post('/uploadToDrive', [GoogleDriveController::class, 'uploadToDrive'])->name('uploadToDrive');
 
 
-Route::get('/application', [DBController::class, 'getPasuc'])->name('application');
-Route::get('/application/{any}', [DBController::class, 'getPasuc'])->where('any', '.*');
-Route::put('/pasuc-update-applied/{pasucId}', [DBController::class, 'pasucUpdateApplied']);
 
-Route::get('/add-documents', [AddController::class, 'addDocuments']);
-Route::get('/add-documents-KRA1', [AddController::class, 'showPAGEaddKRA1']);
-Route::get('/add-documents-KRA2', [AddController::class, 'showPAGEaddKRA2']);
-Route::get('/add-documents-KRA3', [AddController::class, 'showPAGEaddKRA3']);
-Route::get('/add-documents-KRA4', [AddController::class, 'showPAGEaddKRA4']);
+// Route::get('DBController', [DBController::class, 'store']);
+
+// Route::get('/home', [TabController::class, 'home']);
+
+// Route::get('/home', [DBController::class, 'home']);
+// Route::get('/eqar', [DBController::class, 'eqar']);
+// Route::get('/eqar', [DBController::class, 'getEqar'])->name('eqar');
+// Route::put('/eqar-update-applied/{eqarId}', [DBController::class, 'eqarUpdateApplied']);
+// Route::get('/eqarSearch', [SearchController::class, 'eqarSearch']);
+// Route::get('/eqarSort', [SortController::class, 'eqarSort']);
+// Route::get('/pasucSearch', [SearchController::class, 'pasucSearch']);
+// Route::get('/pasucSort', [SortController::class, 'pasucSort']);
+// Route::get('/cycle', [CycleController::class, 'cycle']);
+
+
+// Route::get('/application', [DBController::class, 'getPasuc'])->name('application');
+// Route::get('/application/{any}', [DBController::class, 'getPasuc'])->where('any', '.*');
+// Route::put('/pasuc-update-applied/{pasucId}', [DBController::class, 'pasucUpdateApplied']);
+
+// Route::get('/add-documents', [AddController::class, 'addDocuments']);
+// Route::get('/add-documents-KRA1', [AddController::class, 'showPAGEaddKRA1']);
+// Route::get('/add-documents-KRA2', [AddController::class, 'showPAGEaddKRA2']);
+// Route::get('/add-documents-KRA3', [AddController::class, 'showPAGEaddKRA3']);
+// Route::get('/add-documents-KRA4', [AddController::class, 'showPAGEaddKRA4']);
 
 
