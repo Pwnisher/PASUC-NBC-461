c4ddbb7356fc16305b2b38dafbb2032fca41357b
John Reign Encina <copythatencina24@gmail.com>
2023-08-27 13:41:58 +0800

    Revert "Update application.blade.php"
    This reverts commit e4749f8ac38cddbcca3e326d1a5514e8418d2ed0.

--- resources/views/application.blade.php
+++ resources/views/application.blade.php
@@ -137,7 +137,7 @@
                                 <th class="py-4 px-6 cursor-pointer text-center">Actions</th>
                             </tr>
                         </thead>
-                        <tbody class="divide-y divide-gray-300" id="table-body">
+                        <tbody class="divide-y divide-gray-300">
                           @include('partials.pasucfiltered_table')
                         </tbody>
                     </table>
